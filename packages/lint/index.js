61e55a7f786962a320eaa8141c3be6b5b0fcca7e
mitou <xmitoux@gmail.com>
2024-06-03 22:04:02 +0900

    style: lint index.jsの整理

--- packages/lint/index.js
+++ packages/lint/index.js
@@ -5,7 +5,6 @@ import onlyWarn from 'eslint-plugin-only-warn';
 import typescriptParser from '@typescript-eslint/parser';
 
 /**
- * rules
  * @type {import('eslint').Linter.FlatConfig}
  */
 export const customEslintRules = {
@@ -16,16 +15,15 @@ export const customEslintRules = {
     },
 };
 
-/**
- * dirs and files to ignore
- *
- * @type {import('eslint').Linter.FlatConfig}
- */
 export const customIgnores = {
     ignores: ['**/node_modules', '**/public', '**/vendor', '**/dist', '**/.nuxt'],
 };
 
 const configUnicornRecommended = eslintPluginUnicorn.configs['flat/recommended'];
+
+/**
+ * @type {import('eslint').Linter.FlatConfig}
+ */
 const customUnicornRules = {
     rules: {
         'unicorn/consistent-function-scoping': 'off',
@@ -40,16 +38,7 @@ const configOnlyWarn = {
         'only-warn': onlyWarn,
     },
 };
-/**
- * For usage within a Nuxt context
- *
- * The helper `withNuxt()` from auto-generated `.nuxt/eslint.config.mjs` can be used to append
- * these rules after the config tailored to the Nuxt-app where the eslint config is being used.
- * @see https://eslint.nuxt.com/packages/module
- *
- * It will also add all best-practices rules for js/ts/vue from `@nuxt/eslint-config`.
- * @see https://eslint.nuxt.com/packages/config
- */
+
 export const eslintConfigNuxt = [
     customEslintRules,
     customIgnores,
@@ -71,11 +60,6 @@ const configParser = {
     },
 };
 
-/**
- * For standalone usage in packages without Nuxt context.
- * `createConfigForNuxt()` will create all best-practices rules for js/ts/vue from `@nuxt/eslint-config`.
- * @see https://eslint.nuxt.com/packages/config
- */
 export const eslintConfigStandalone = createConfigForNuxt().append([
     customEslintRules,
     customIgnores,
