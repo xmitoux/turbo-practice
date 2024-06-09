bab18ceccd6ffb64ba9a346881ed8ff102772444
mitou <xmitoux@gmail.com>
2024-06-09 11:50:16 +0900

    database: iamge変更

---
+++ apps/backend/prisma/migrations/20240609014803_drop_coulumn/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `file_url` on the `images` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "images" DROP COLUMN "file_url";
