865571c1fdaa77b0090611752f230a853b3daacf
mitou <xmitoux@gmail.com>
2024-06-22 15:25:05 +0900

    chore(front): PWA manifestを本番環境のみ読み込む

--- apps/frontend/app.vue
+++ apps/frontend/app.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
+const NuxtPwaManifest = process.env.NODE_ENV === 'production'
+  ? resolveComponent('NuxtPwaManifest')
+  : 'div';
 </script>
 
 <template>
   <div>
-    <NuxtPwaManifest />
+    <component :is="NuxtPwaManifest" />
     <NuxtPage />
   </div>
 </template>
