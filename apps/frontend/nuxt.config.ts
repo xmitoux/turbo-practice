5d1075a8aee72ae63f49e5152f34c74a4570c857
mitou <xmitoux@gmail.com>
2024-06-22 10:18:54 +0900

    install: nuxt PWA
    https://vite-pwa-org.netlify.app/frameworks/nuxt

--- apps/frontend/nuxt.config.ts
+++ apps/frontend/nuxt.config.ts
@@ -5,6 +5,12 @@ const devModules = process.env.NODE_ENV === 'development'
   ? ['@nuxt/eslint']
   : [];
 
+const modules = [
+  'nuxt-quasar-ui',
+  '@vite-pwa/nuxt',
+  ...devModules,
+];
+
 export default defineNuxtConfig({
   devServer: {
     // https://ja.vitejs.dev/config/server-options
@@ -17,10 +23,7 @@ export default defineNuxtConfig({
       stylistic: true,
     },
   },
-  modules: [
-    'nuxt-quasar-ui',
-    ...devModules,
-  ],
+  modules,
   runtimeConfig: {
     public: {
       baseURL: process.env.BASE_API_URL,
